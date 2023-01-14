eee9a0f5ba84b3d6025101972cc7df38c97369e1
Leo-Phyo-Hein <leo_phyo_hein@s2017.sst.edu.sg>
2023-01-14 16:05:50 +0800

    added icons, fixed interval

--- popup.js
+++ popup.js
@@ -1,19 +1,24 @@
+var popupPresent = false;
+
 // Function to create and display the captcha popup
 function showCaptchaPopup() {
 
     // Generate a random string of alphabets and numbers
     var randomString = Math.random().toString(36).slice(-8);
 
-    // Create the popup element
-    var popup = document.createElement("div");
-    popup.innerHTML = `Please enter the captcha ${randomString}: ` + "<input type='text' id='captcha-input'/>";
-    popup.style.backgroundColor = "red";
-    popup.style.padding = "20px";
-    popup.style.position = "fixed";
-    popup.style.top = "50%";
-    popup.style.left = "50%";
-    popup.style.transform = "translate(-50%, -50%)";
-    popup.style.zIndex = "1000";
+    if (!popupPresent) {
+        // Create the popup element
+        var popup = document.createElement("div");
+        popup.innerHTML = `Please enter the captcha ${randomString}: ` + "<input type='text' id='captcha-input'/>";
+        popup.style.backgroundColor = "red";
+        popup.style.padding = "20px";
+        popup.style.position = "fixed";
+        popup.style.top = "50%";
+        popup.style.left = "50%";
+        popup.style.transform = "translate(-50%, -50%)";
+        popup.style.zIndex = "1000";
+        popupPresent = true;
+    }
     
     // Add the popup to the page
     document.body.appendChild(popup);
