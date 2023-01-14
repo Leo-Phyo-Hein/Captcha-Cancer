a2c7711c92ce93c9e02b4d102743ac31920cf4e9
Trustin <68576751+trwstin@users.noreply.github.com>
2023-01-14 15:59:13 +0800

    Update popup.js
    now generates captcha every 5s in the input field

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 // Function to create and display the captcha popup
 function showCaptchaPopup() {
 
+    // Generate a random string of alphabets and numbers
+    var randomString = Math.random().toString(36).slice(-8);
+
     // Create the popup element
     var popup = document.createElement("div");
-    popup.innerHTML = "Please enter the captcha: <input type='text' id='captcha-input'/>";
+    popup.innerHTML = `Please enter the captcha ${randomString}: ` + "<input type='text' id='captcha-input'/>";
     popup.style.backgroundColor = "red";
     popup.style.padding = "20px";
     popup.style.position = "fixed";
@@ -11,19 +14,12 @@ function showCaptchaPopup() {
     popup.style.left = "50%";
     popup.style.transform = "translate(-50%, -50%)";
     popup.style.zIndex = "1000";
-
+    
     // Add the popup to the page
     document.body.appendChild(popup);
-    disableScroll;
-
-    // Captcha validation
-    // Generate random string of characters then validate
-    if (captcha-input == 'hello') {
-        popup.style.backgroundColor = 'green';
-    }
-    
 }
 
-// Show the captcha popup every 5 seconds
+
+// Show the captcha popup every 10 seconds
 // Will refresh and clear input field if never type quickly enough
 setInterval(showCaptchaPopup, 5000);
